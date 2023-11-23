ec7af67ea3505a23328e2ff70b8e4948431e49ab
d42null <d2null@gmail.com>
2023-11-23 20:20:54 +0000

    Deploying to gh-pages from @ d42null/goit-react-hw-04-feedback@a899cc57f29a1df232ad82f2c88326b52fff1a70 🚀

--- src/components/Notification/Notification.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Message = styled.p`
-  font-size: 24px;
-  font-weight: 500;
-  margin: 36px;
-  color: #666;
-`;
--- src/components/Statistics/Statistics.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-export const Stat = styled.p`
-  font-size: 24px;
-  font-weight: 400;
-  text-align: center;
-`;
